1e9b05464c8d83f466e6fbe0663b121d9639150d
aym1n <ayman.jemel@polytechnique.edu>
2016-11-07 14:13:29 +0100

    Add files via upload
    correction d'une faute

--- max_dist.R
+++ max_dist.R
@@ -1,5 +1,5 @@
 #source("D:/P3a/P3a_2.R")
-#source("D:/P3A/P3a-master/generate_dist.R")
+source("D:/P3A/generate_dist.R")
 
 #la fonction max_dist est l'implémentation de l'algorithme qui à partir de la matrice des distances 
 #renvoie les éléments les plsu éloignés
@@ -12,7 +12,7 @@ max_dist=function(d, N,k){
       num_chosen <- p
       
       chosen <- rep(0,N)
-      chosen[num_chosen] <- 1
+      chosen[num_chosen] <- 1# initialement a contient que num_chosen
       
       a <- rep(0, k)
       a[1] <- num_chosen
@@ -24,7 +24,7 @@ max_dist=function(d, N,k){
       }
       
       for( i in 2:k){
-          far <- 1
+          far <- 1 # ontrouve l'element le plus loin de l'ensemble deja choisis
           dist <- -1
           for( j in 1:N){
               if(chosen[j]==0 & dist<dis_to_set[j]){
@@ -34,17 +34,17 @@ max_dist=function(d, N,k){
           }
           a[i] <- far
           chosen[far]<-1
-          for( j in 1:N){
+          for( j in 1:N){ # on actualise les distances vers l'ensemble choisis
             if(chosen[j]==0 ){
                 dis_to_set[j] <- min(dis_to_set[j],d[j,far])
             }
           }
           
       }
-      current_dist <- -1
-      for ( i in 1:k){
+      current_dist <- d[a[1],a[2]]
+      for ( i in 1:k){ # on calcule la distance minimale entre deux elements quelquonques de l'ensemble choisis
           for(j in 1:k){
-              if(d[a[i],a[j]]>current_dist){
+              if(d[a[i],a[j]]<current_dist){
                 current_dist <- d[a[i],a[j]]
               }  
           }
